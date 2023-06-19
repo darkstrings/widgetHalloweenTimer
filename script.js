67ab14294cb3d1002a6912840c86d715ba659b80
Lucien Gaydos <darkstrings@gmx.com>
2023-06-19 18:10:49 -0400

    first commit

---
+++ script.js
@@ -0,0 +1,30 @@
+const halloween = "31 Oct 2023";
+
+const daysDisplay = document.querySelector("#days");
+const hoursDisplay = document.querySelector("#hours");
+const minutesDisplay = document.querySelector("#minutes");
+const secondsDisplay = document.querySelector("#seconds");
+
+function countdown() {
+  const halloweenDate = new Date(halloween);
+  const currentDate = new Date(); //these are of course in milliseconds
+
+  const totalSeconds = (halloweenDate - currentDate) / 1000; //converts milliseconds to whole seconds
+
+  const days = Math.floor(totalSeconds / 3600 / 24);
+
+  const hours = Math.floor(totalSeconds / 3600) % 24;
+
+  const minutes = Math.floor(totalSeconds / 60) % 60;
+
+  const seconds = Math.floor(totalSeconds % 60);
+
+  daysDisplay.textContent = days;
+  hoursDisplay.textContent = hours;
+  minutesDisplay.textContent = minutes;
+  secondsDisplay.textContent = seconds;
+}
+
+countdown();
+
+setInterval(countdown, 1000);
